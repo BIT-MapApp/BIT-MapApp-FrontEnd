b23bde21bed6c6a7de47b0ef33d671d310027e96
wuzirui <wuzirui@tztyun.org>
2021-11-02 00:05:17 +0800

    添加底部选项卡，但是现在还没有切换逻辑

--- flutter_src/lib/main.dart
+++ flutter_src/lib/main.dart
@@ -13,42 +13,69 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: appName,
       theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.lightGreen[900],
-          backgroundColor: Colors.yellow,
+        brightness: Brightness.light,
+        primaryColor: Colors.lightGreen[900],
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: Colors.yellow),
       ),
       home: HomePage(title: appName),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String title;
 
-  HomePage({Key? key, required this.title}) : super(key: key);
-  
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePage();
+  }
+}
+
+class _HomePage extends State<HomePage> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         backgroundColor: Theme.of(context).primaryColor,
       ),
       body: Center(
-        child: Container(
-          color: Theme.of(context).secondaryHeaderColor,
-          child: Text(
-            'with background',
-            style: Theme.of(context).textTheme.caption
-          ),
-        )
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('with background',
+              style: Theme.of(context).textTheme.caption),
+            Text('$_counter time clicked', style: Theme.of(context).textTheme.caption)
+          ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: '地图'),
+          BottomNavigationBarItem(icon: Icon(Icons.art_track), label: '动态'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: '个人信息')
+
+        ],
       ),
       floatingActionButton: Theme(
-        data: Theme.of(context).copyWith(colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.grey)),
-        child: const FloatingActionButton(onPressed: null, child: Icon(Icons.computer))
-      ),
+          data: Theme.of(context).copyWith(
+              colorScheme:
+                  ColorScheme.fromSwatch().copyWith(secondary: Colors.grey)),
+          child: FloatingActionButton(
+              onPressed: _inc,
+              tooltip: 'Increment',
+              child: const Icon(Icons.computer))),
     );
   }
 
+  void _inc() {
+    setState(() {
+      // 设置计数器变量
+      _counter++;
+    });
+  }
 }
-
